6208f0cdd7303dcc2a7d381bf5287c021c156ede
jiayuliang <jiayuliang@kanzhun.com>
2021-04-30 16:37:24 +0800

    feat(scanJar):scanJar

--- app/src/main/java/com/kite/testplugin/MainActivity.java
+++ app/src/main/java/com/kite/testplugin/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.better_better_better_better_activity_main);
+        setContentView(R.layout.better_better_better_better_better_activity_main);
     }
 }
--- myplugin/src/main/groovy/com/bigman/plugin/RegisterTransform.groovy
+++ myplugin/src/main/groovy/com/bigman/plugin/RegisterTransform.groovy
@@ -4,6 +4,8 @@ import com.android.build.api.transform.*
 import com.android.build.gradle.internal.pipeline.TransformManager
 import org.gradle.api.Project
 
+import java.util.jar.JarFile
+
 class RegisterTransform extends Transform {
     Project mProject
 
@@ -40,13 +42,30 @@ class RegisterTransform extends Transform {
             TransformInput input ->
                 input.jarInputs.each {
                     JarInput jarInput ->
-                        mProject.logger.warn("jar---" + jarInput.file.absolutePath)
+//                        mProject.logger.warn("jar---" + jarInput.file.absolutePath)
+                        scanJar(jarInput.file)
                 }
 
                 input.directoryInputs.each {
-                    DirectoryInput directoryInput->
+                    DirectoryInput directoryInput ->
                         mProject.logger.warn("dir file ---" + directoryInput.file.absolutePath)
                 }
+        }
+    }
+
+    private void scanJar(File file) {
+        def jarFile = new JarFile(file)
+        def enumeration = jarFile.entries()
+        while (enumeration.hasMoreElements()) {
+            def jarEntry = enumeration.nextElement()
+            def entryName = jarEntry.name
+            if(entryName.startsWith("androidx/") || entryName.startsWith("android/") || entryName.startsWith("META-INF/")){
+                break;
+            }
+            mProject.logger.error("jar---" + file.absolutePath + " class --- " + entryName)
+        }
+        if (jarFile != null) {
+            jarFile.close()
         }
     }
 }
